6177267f8aee2d485ee9795d2e076b7041cf792c
gidyon <gideon.ngk@gmail.com>
2022-02-18 05:45:09 +0300

    updated list filters

--- pkg/api/phonebook/v1/phones.go
+++ pkg/api/phonebook/v1/phones.go
@@ -4,18 +4,17 @@ package phonebook
 import "context"
 
 type PhoneBookService interface {
-	CreatePhoneRecord(context.Context, PhoneRecord) error
-	GetPhoneRecord(context.Context, GetPhoneRecordRequest) (PhoneRecord, error)
-	ListPhoneRecords(context.Context, ListPhoneRecordsRequest) (ListPhoneRecordsResponse, error)
-	DeletePhoneRecord(context.Context, DeletePhoneRecordRequest) error
+	CreatePhoneRecord(context.Context, *PhoneRecord) error
+	GetPhoneRecord(context.Context, *GetPhoneRecordRequest) (*PhoneRecord, error)
+	ListPhoneRecords(context.Context, *ListPhoneRecordsRequest) (*ListPhoneRecordsResponse, error)
+	DeletePhoneRecord(context.Context, *DeletePhoneRecordRequest) error
 }
 
 type PhoneRecord struct {
 	Id          string `json:"id,omitempty"`
-	CustName    string `json:"cust_name,omitempty"`
 	CustId      string `json:"cust_id,omitempty"`
 	CountryName string `json:"country_name,omitempty"`
-	CountryCode string `json:"country_code,omitempty"`
+	CountryCode uint   `json:"country_code,omitempty"`
 	Number      string `json:"number,omitempty"`
 	PhoneValid  bool   `json:"phone_valid,omitempty"`
 	CreateDate  string `json:"create_date,omitempty"`
@@ -32,15 +31,16 @@ type ListPhoneRecordsRequest struct {
 }
 
 type PhoneRecordsFilters struct {
-	CountryCode  int    `json:"country_code,omitempty"`
-	ValidOnly    int    `json:"valid_only,omitempty"`
+	CountryCode  string `json:"country_code,omitempty"`
+	ValidOnly    bool   `json:"valid_only,omitempty"`
 	NotValidOnly bool   `json:"not_valid_only,omitempty"`
 	PhoneNumber  string `json:"phone_number,omitempty"`
 }
 
 type ListPhoneRecordsResponse struct {
-	PhoneRecords  []*PhoneRecord `json:"phone_records,omitempty"`
-	NextPageToken string         `json:"next_page_token,omitempty"`
+	PhoneRecords    []*PhoneRecord `json:"phone_records,omitempty"`
+	NextPageToken   string         `json:"next_page_token,omitempty"`
+	CollectionCount int32          `json:"collection_count,omitempty"`
 }
 
 type DeletePhoneRecordRequest struct {
